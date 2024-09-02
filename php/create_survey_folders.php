a417f817954004048bdfa7ca173fc4f25b4de17c
Bernhard Friedrichs <bernhard.friedrichs@metronix.de>
2024-09-02 15:18:02 +0200

    php changes

--- php/create_survey_folders.php
+++ php/create_survey_folders.php
@@ -1,20 +1,19 @@
 #!/usr/bin/php
 <?php
-$subdirs = array("config", "db", "dump", "edi", "filters", "jle", "jobs", "log", "processings", "shell", "tmp", "stations", "reports");
+$subdirs = array("config", "db", "dump", "edi", "filters", "jle", "jobs", "log", "meta", "processings", "shell",  "stations", "tmp");
 #
 #
-if ($argc < 2 ) {
-    exit( "Usage: $argv[0] folder_name" . PHP_EOL );
-}
-else {
- if (!is_dir("$argv[1]") ) mkdir("$argv[1]");
+if ($argc < 2) {
+  exit("Usage: $argv[0] folder_name" . PHP_EOL);
+} else {
+  if (!is_dir("$argv[1]")) mkdir("$argv[1]");
 }
 #
 if (is_dir("$argv[1]")) {
   foreach ($subdirs as $subdir) {
     $cdir = "$argv[1]" . "/" . "$subdir";
     echo "creating " . $cdir . PHP_EOL;
-    if (!is_dir("$cdir") ) mkdir("$cdir");
+    if (!is_dir("$cdir")) mkdir("$cdir");
   }
 }
 #
--- php/include/atss_file.php
+++ php/include/atss_file.php
@@ -1,59 +1,97 @@
 <?php
-
 if (!isset($_SESSION)) session_start();
-class atss_file {
-  function __construct($channel_no, $serial = 0, $system = 0, $run = 0, $channel_type = 0, $sample_rate = 0) {
 
-    $this->channel_no = intval($channel_no);
+class atss_calibration {
+  public $sensor;
+  public $serial;
+  public $chopper;
+  public $units_frequency;
+  public $units_amplitude;
+  public $units_phase;
+  public $datetime;
+  public $Operator;
+  public $f = array();
+  public $a = array();
+  public $p = array();
 
-    if(!isset($_SESSION["adu"][$this->channel_no]["atss_file"]["channel_type"])) {
-      $this->clear();
-      $this->serial = $serial;
-      $this->system = $system;
-      $this->channel_no = intval($channel_no);
-      $this->run = $run;
-      $this->channel_type = $channel_type;
-      $this->sample_rate = floatval($sample_rate);
-
-      $this->vars_to_session();
-    }
-    else $this->session_to_vars();
-
-  }
-  function vars_to_session() {
-    if(!isset($_SESSION["adu"][$this->channel_no]["atss_file"]["serial"]))       $_SESSION["adu"][$this->channel_no]["atss_file"]["serial"] = intval($this->serial);
-    if(!isset($_SESSION["adu"][$this->channel_no]["atss_file"]["system"]))       $_SESSION["adu"][$this->channel_no]["atss_file"]["system"] = $this->system;
-    if(!isset($_SESSION["adu"][$this->channel_no]["atss_file"]["channel_no"]))   $_SESSION["adu"][$this->channel_no]["atss_file"]["channel_no"] = intval($this->channel_no);
-    if(!isset($_SESSION["adu"][$this->channel_no]["atss_file"]["run"]))          $_SESSION["adu"][$this->channel_no]["atss_file"]["run"] = intval($this->run);
-    if(!isset($_SESSION["adu"][$this->channel_no]["atss_file"]["channel_type"])) $_SESSION["adu"][$this->channel_no]["atss_file"]["channel_type"] = $this->channel_type;
-    if(!isset($_SESSION["adu"][$this->channel_no]["atss_file"]["sample_rate"]))  $_SESSION["adu"][$this->channel_no]["atss_file"]["sample_rate"] = floatval($this->sample_rate);
-  }
-
-  function session_to_vars() {
-    if(isset($_SESSION["adu"][$this->channel_no]["atss_file"]["serial"]))            $this->serial =       intval($_SESSION["adu"][$this->channel_no]["atss_file"]["serial"]);
-    if(isset($_SESSION["adu"][$this->channel_no]["atss_file"]["system"]))            $this->system =       $_SESSION["adu"][$this->channel_no]["atss_file"]["system"];
-    if(isset($_SESSION["adu"][$this->channel_no]["atss_file"]["channel_no"]))        $this->channel_no =   intval($_SESSION["adu"][$this->channel_no]["atss_file"]["channel_no"]);
-    if(isset($_SESSION["adu"][$this->channel_no]["atss_file"]["run"]))               $this->run =          intval($_SESSION["adu"][$this->channel_no]["atss_file"]["run"]);
-    if(isset($_SESSION["adu"][$this->channel_no]["atss_file"]["channel_type"]))      $this->channel_type = $_SESSION["adu"][$this->channel_no]["atss_file"]["channel_type"];
-    if(isset($_SESSION["adu"][$this->channel_no]["atss_file"]["sample_rate"]))       $this->sample_rate =  floatval($_SESSION["adu"][$this->channel_no]["atss_file"]["sample_rate"]);
-  }
-
-  function clear() {
-    $this->serial = 0;
-    $this->system = "";
-    $this->channel_no = -1;
-    $this->run = 0;
-    $this->channel_type = 0;
-    $this->sample_rate = 0.0;
-  }
-
-  function get_channel_type() {
-    return $this->channel_type;
-  }
-
-  function get_sample_rate() {
-    return floatval($this->sample_rate);
+  function __construct($sensor = "", $serial = 0, $chopper = 0, $units_frequency = "Hz", $units_amplitude = "mV/nT", $units_phase = "degrees", $datetime = "1970-01-01T00:00:00", $Operator = "") {
+    $this->sensor = $sensor;
+    $this->serial = intval($serial);
+    $this->chopper = intval($chopper);
+    $this->units_frequency = $units_frequency;
+    $this->units_amplitude = $units_amplitude;
+    $this->units_phase = $units_phase;
+    $this->datetime = $datetime;
+    $this->Operator = $Operator;
+    $this->f = array();
+    $this->a = array();
+    $this->p = array();
   }
 }
 
-?>
+class atss_file_tags {
+
+  public $serial;
+  public $system;
+  public $channel_no;
+  public $channel_type;
+  public $sample_rate;
+
+  function __construct($serial = 0, $system = '', $channel_no = 0, $channel_type = '', $sample_rate = 0.0) {
+    $this->serial = intval($serial);
+    $this->system = $system;
+    $this->channel_no = intval($channel_no);
+    $this->channel_type = $channel_type;
+    $this->sample_rate = floatval($sample_rate);
+  }
+}
+
+class atss_header {
+  public $datetime; // ISO 8601 datetime in UTC
+  public $latitude; // decimal degree such as 52.2443
+  public $longitude; // decimal degree such as 10.5594
+  public $elevation; // elevation in meter
+  public $azimuth; //  orientation from North to East (90 = East, -90 or 270 = West, 180 South, 0 North)
+  public $tilt; //  azimuth positive down - in case it had been measured
+  public $resistance; //  resistance of the sensor in Ohm or contact resistance of electrode in Ohm
+  public $units; //  for ADUs it will be mV H or other -  or scaled E mV/km (the logger will do this while recording)
+  public $filter; //  comma separated list of filters such as "ADB-LF,LF-RF-4" or "ADB-HF,HF-RF-1"
+  public $source; //  empty or indicate as, ns, ca, cp, tx or what ever; some users need this
+
+  function __construct($datetime = '1970-01-01T00:00:00.0', $latitude = 0.0, $longitude = 0.0, $elevation = 0.0, $azimuth = 0.0, $tilt = 0.0, $resistance = 0.0, $units = 'mV', $filter = '', $source = '') {
+    $this->datetime = $datetime;
+    $this->latitude = floatval($latitude);
+    $this->longitude = floatval($longitude);
+    $this->elevation = floatval($elevation);
+    $this->azimuth = floatval($azimuth);
+    $this->tilt = floatval($tilt);
+    $this->resistance = floatval($resistance);
+    $this->units = $units;
+    $this->filter = $filter;
+    $this->source = $source;
+  }
+
+  public function get_date(): string {
+    return substr($this->datetime, 0, 10);
+  }
+
+  public function get_time(): string {
+    return substr($this->datetime, 11, 8);
+  }
+
+  public function get_timestamp(): int {
+    return strtotime($this->datetime);
+  }
+}
+
+class atss_channel {
+  public $file_tags;
+  public $header;
+  public $sensor_calibration;
+
+  function __construct($file_tags = null, $header = null, $sensor_calibration = null) {
+    $this->file_tags = $file_tags;
+    $this->header = $header;
+    $this->sensor_calibration = $sensor_calibration;
+  }
+}
